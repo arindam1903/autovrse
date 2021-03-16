b969e62572246d5bf644262c1bcacfed777ad6cd
Arindam dutta <arindam408@gmail.com>
2021-03-16 18:50:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useRef} from 'react';
+import {Canvas, useFrame} from 'react-three-fiber';
+import  {OrbitControls} from '@react-three/drei';
+import './index.css';
+
+const Sphere=()=>{
+  const mesh=useRef();
+  useFrame(()=>(mesh.current.rotation.x=mesh.current.rotation.y+=0.01));
+  
+  return(
+    
+    <mesh ref={mesh} position={[4,0,0]}>
+    <sphereBufferGeometry attach='geometry' args={[1,32,32]}/>
+  
+    <meshStandardMaterial attach='material' color="purple" />
+  </mesh>
+  )
+}
+const Box=()=>{
+  const mesh=useRef();
+  useFrame(()=>(mesh.current.rotation.x=mesh.current.rotation.y+=0.01));
+  
+  return(
+    
+    <mesh ref={mesh}>
+    <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
+  
+    <meshStandardMaterial attach='material' color="red" />
+  </mesh>
+  )
+}
+const Cylinder=()=>{
+  const mesh=useRef();
+  useFrame(()=>(mesh.current.rotation.x=mesh.current.rotation.y+=0.01));
+  
+  return(
+    
+    <mesh ref={mesh} position={[-4,0,0]}>
+    <cylinderBufferGeometry attach='geometry' args={[1,1,1,500]}/>
+  
+    <meshStandardMaterial attach='material' color="green" />
+  </mesh>
+  )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  
+  
+  return (<>
+  <button id='sphere-button'>hello</button>
+  <Canvas>
+ <Sphere/>
+ <Box/>
+      <Cylinder/>
+      <ambientLight intensity={.1}/>
+      <pointLight position={[10,0,10]}intensity={1}/>
+      <pointLight position={[-10,0,10]} intensity={.5}/>
+      <pointLight position={[-10,0,-10]} intensity={1}/>
+  
+   <OrbitControls/>
+    </Canvas>  
+ 
+    
+   
+    </>
   );
 }
 
